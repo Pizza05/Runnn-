3f207ce139890f4e0381ddf60e05157d72e772c5
Pizza05 <150358173+Pizza05@users.noreply.github.com>
2025-03-26 09:32:39 +0700

    Add files via upload

---
+++ Emnemies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class Emnemies : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ทำให้Enemy จะไล่ตามผู้เล่น
+        enemy.SetDestination(Player.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //เมื่อEnemy เช้าใกล้ player player จะทำลายคัวเอง
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
