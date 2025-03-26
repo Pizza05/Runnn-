852d0dd64f0cb598ad4d9e541c407d0200ee8168
Pizza05 <150358173+Pizza05@users.noreply.github.com>
2025-03-26 09:33:04 +0700

    Add files via upload

---
+++ PlayerInventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public float speed;
+    public TMPro.TextMeshPro labalCoin;
+
+    private int coin;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Coin") // tag coin เพื่อ Object อื่่นที่ไม่ใช่ coin มาทำลายตัวเอง
+        {
+            Destroy(other.gameObject);
+
+            coin++;
+
+            labalCoin.text = coin.ToString();
+        }
+    }
+}
